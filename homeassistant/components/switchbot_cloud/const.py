3463cd9cc1e5b566c1cb4957e059b971a599910a
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-06-07 13:21:49 +0800

    reset it back

--- homeassistant/components/switchbot_cloud/const.py
+++ homeassistant/components/switchbot_cloud/const.py
@@ -21,6 +21,10 @@ DOMAIN: Final = "switchbot_cloud"
 ENTRY_TITLE = "SwitchBot Cloud"
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=600)
 
+SENSOR_KIND_TEMPERATURE = "temperature"
+SENSOR_KIND_HUMIDITY = "humidity"
+SENSOR_KIND_BATTERY = "battery"
+
 VACUUM_FAN_SPEED_QUIET = "quiet"
 VACUUM_FAN_SPEED_STANDARD = "standard"
 VACUUM_FAN_SPEED_STRONG = "strong"
